b8a7559bfbd9cd919478a9d5c5139d300c1c7965
mac <macdev2001@gmail.com>
2023-09-09 13:26:58 +0700

    Some feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Paragraph from './Paragraph';
 import Link from './Link';
+import Clock from './Clock';
 
 function App() {
   return (
@@ -8,8 +9,14 @@ function App() {
         <div className="text-6xl mb-3">:)</div>
         <h1 className="mb-2 text-2xl md:text-4xl font-bold mb-4">There is no problem.</h1>
         <Paragraph text="Hey there, I'm Mac, your everyday normal guy from Earth" />
-        <div className="mt-4"><b>BIRTHDATE:</b> 3rd April 2008</div>
+        <div className="mt-4 text-lg"><b>Untechnical Information:</b></div>
+        <div className="">
+        <div className=""><b>BIRTHDATE:</b> 3rd April 2008</div>
+        <div><b>FAV-SONG:</b> <Link text="night sky patrol of tomorrow" link="https://youtu.be/XogSflwXgpw"/> by orangestar</div>
         <div><b>CONTACTS:</b> <Link text="Instagram" link="https://instagram.com/bluestar.name" />, <Link text="Discord" link="https://discord.com/users/829156179803504670" /></div>
+        <Clock />
+        </div>
+
       </div>
     </div>
   );
