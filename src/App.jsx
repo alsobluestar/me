eec94cf1c3706b12569a6d0b61bde158e20cce03
Mac <macdev2001@gmail.com>
2023-09-18 18:36:22 +0700

    some added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,55 @@
+import { motion } from 'framer-motion';
 import Paragraph from './Paragraph';
 import Link from './Link';
 import Clock from './Clock';
+import Logtext from './Logtext';
+
+function App() {
+  return (
+    <div className="mx-auto p-8 mt-16 md:mt-28 w-full h-screen text-white">
+      <div>
+       <motion.div
+           whileTap={{
+               scale: 1.2,
+               }}>
+
+        <Link text={ <div className="text-white w-fit text-6xl mb-3">:)</div>} link=""/>
+
+       </motion.div>
+
+        <h1 className="mb-2 text-2xl md:text-4xl font-bold mb-4">There is no problem.</h1>
+        <Logtext title="Hey there, I'm Mac, your everyday normal guy from Earth" content="" />
+        <div className="mt-4 text-lg"><b>Untechnical Information:</b></div>
+        <Logtext title="BIRTHDATE:" content="3rd April 2008" />
+        <Logtext
+          title="FAV-SONG:"
+          content={<Link text="night sky patrol of tomorrow" link="https://youtu.be/XogSflwXgpw" />}
+        />
+        <Logtext
+          title="CONTACTS:"
+          content={
+            <>
+              <Link text="Instagram" link="https://instagram.com/bluestar.name" />,
+
+              <Link text="Discord" link="https://discord.com/users/829156179803504670" />
+            </>
+          }
+        />
+        <Clock />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+{/*
+import Paragraph from './Paragraph';
+import Link from './Link';
+import Clock from './Clock';
+import Logtext from './Logtext';
 
 function App() {
   return (
@@ -14,6 +63,7 @@ function App() {
         <div className=""><b>BIRTHDATE:</b> 3rd April 2008</div>
         <div><b>FAV-SONG:</b> <Link text="night sky patrol of tomorrow" link="https://youtu.be/XogSflwXgpw"/> by orangestar</div>
         <div><b>CONTACTS:</b> <Link text="Instagram" link="https://instagram.com/bluestar.name" />, <Link text="Discord" link="https://discord.com/users/829156179803504670" /></div>
+        <Logtext title="TEST:" content="test" />
         <Clock />
         </div>
 
@@ -23,6 +73,4 @@ function App() {
 }
 
 export default App;
-
-
-
+*/}
