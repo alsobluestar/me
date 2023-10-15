9b09db1f15c6c52c1b35ea05d2b41851fa0d167d
mac <bluestardotos@gmail.com>
2023-10-15 21:34:23 +0700

    New update(design)

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,31 @@
-import Paragraph from './Paragraph';
-import Link from './Link';
-import Clock from './Clock';
-import Logtext from './Logtext';
+const App = () => {
+  const containerClass = "bg-zinc-800 min-h-screen flex flex-col items-center justify-center p-1";
+  const cardClass = "bg-zinc-300 mb-2 p-6 rounded-lg shadow-lg shadow-zinc-400 text-center border-2 border-zinc-900 w-full md:w-2/3 lg:w-1/2"
+  const sectionTitleClass = "text-xl font-bold mb-4";
+  const cardContentClass = "text-zinc-900 font-semibold";
 
-function App() {
   return (
-    <div className="mx-auto p-8 mt-16 md:mt-28 w-full h-screen text-white">
-      <div>
-    <Link text={ <div className="text-white w-fit text-6xl mb-3">:)</div>} link="https://discord.gg/uSGVMe9Qmm"/>
+    <div className={containerClass}>
+      <header className={cardClass}>
+        <h1 className={sectionTitleClass}>Hey, I'm Mac <span className="text-sm">(aka bluestar)</span></h1>
+        <p className={cardContentClass}>Just a non formal student who enjoys all sorts of stuff. 🚀</p>
+      </header>
 
+      <div className={cardClass}>
+        <h2 className={sectionTitleClass}>A Bit About Me</h2>
+        <p className={cardContentClass}>Born on April 3, 2008.</p>
+        <p className={cardContentClass}>I've tinkered with Python for 5 years, played around with JavaScript for a year, dabbled in Golang for a month, and had a taste of C. 🖥️</p>
+      </div>
 
-        <h1 className="mb-2 text-2xl md:text-4xl font-bold mb-4">There is no problem.</h1>
-        <Logtext title="Hey there, I'm Mac, your everyday normal guy from Earth" content="" />
-        <div className="mt-4 text-lg"><b>Untechnical Information:</b></div>
-        <Logtext title="BIRTHDATE:" content="3rd April 2008" />
-        <Logtext
-          title="FAV-SONG:"
-          content={<Link text="night sky patrol of tomorrow" link="https://youtu.be/XogSflwXgpw" />}
-        />
-        <Logtext
-          title="CONTACTS:"
-          content={
-            <>
-              <Link text="Instagram" link="https://instagram.com/bluestar.name" />,
+      <div className={cardClass}>
+        <h2 className={sectionTitleClass}>What I'm Into</h2>
+        <p className={cardContentClass}>I enjoy network and scripting programming. 🌐</p>
+        <p className={cardContentClass}>I'm pretty fascinated by aerospace and space science. 🌌</p>
+      </div>
 
-              <Link text="Discord" link="https://discord.com/users/829156179803504670" />
-              
-            </>
-          }
-        />
-
-        <Logtext
-          title="SOURCE:"
-          content={
-            <>
-              <Link text="GITHUB" link="https://github.com/alsobluestar/me" />
-
-            </>
-          }
-        />
-
-        <Clock />
+      <div className={cardClass}>
+        <h2 className={sectionTitleClass}>What Stupid Things I Do</h2>
+        <p className={cardContentClass}>Well, I have a quirky side. Sometimes, I like to 'flood' websites. (bruh💀)</p>
       </div>
     </div>
   );
@@ -50,32 +35,4 @@ export default App;
 
 
 
-{/*
-import Paragraph from './Paragraph';
-import Link from './Link';
-import Clock from './Clock';
-import Logtext from './Logtext';
 
-function App() {
-  return (
-    <div className="mx-auto p-8 mt-16 md:mt-28 w-full h-screen text-white">
-      <div>
-        <div className="text-6xl mb-3">:)</div>
-        <h1 className="mb-2 text-2xl md:text-4xl font-bold mb-4">There is no problem.</h1>
-        <Paragraph text="Hey there, I'm Mac, your everyday normal guy from Earth" />
-        <div className="mt-4 text-lg"><b>Untechnical Information:</b></div>
-        <div className="">
-        <div className=""><b>BIRTHDATE:</b> 3rd April 2008</div>
-        <div><b>FAV-SONG:</b> <Link text="night sky patrol of tomorrow" link="https://youtu.be/XogSflwXgpw"/> by orangestar</div>
-        <div><b>CONTACTS:</b> <Link text="Instagram" link="https://instagram.com/bluestar.name" />, <Link text="Discord" link="https://discord.com/users/829156179803504670" /></div>
-        <Logtext title="TEST:" content="test" />
-        <Clock />
-        </div>
-
-      </div>
-    </div>
-  );
-}
-
-export default App;
-*/}
---
+++ src/Archive/App.jsx
@@ -0,0 +1,81 @@
+import Paragraph from './Paragraph';
+import Link from './Link';
+import Clock from './Clock';
+import Logtext from './Logtext';
+
+function App() {
+  return (
+    <div className="mx-auto p-8 mt-16 md:mt-28 w-full h-screen text-white">
+      <div>
+    <Link text={ <div className="text-white w-fit text-6xl mb-3">:)</div>} link="https://discord.gg/uSGVMe9Qmm"/>
+
+
+        <h1 className="mb-2 text-2xl md:text-4xl font-bold mb-4">There is no problem.</h1>
+        <Logtext title="Hey there, I'm Mac, your everyday normal guy from Earth" content="" />
+        <div className="mt-4 text-lg"><b>Untechnical Information:</b></div>
+        <Logtext title="BIRTHDATE:" content="3rd April 2008" />
+        <Logtext
+          title="FAV-SONG:"
+          content={<Link text="night sky patrol of tomorrow" link="https://youtu.be/XogSflwXgpw" />}
+        />
+        <Logtext
+          title="CONTACTS:"
+          content={
+            <>
+              <Link text="Instagram" link="https://instagram.com/bluestar.name" />,
+
+              <Link text="Discord" link="https://discord.com/users/829156179803504670" />
+              
+            </>
+          }
+        />
+
+        <Logtext
+          title="SOURCE:"
+          content={
+            <>
+              <Link text="GITHUB" link="https://github.com/alsobluestar/me" />
+
+            </>
+          }
+        />
+
+        <Clock />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+{/*
+import Paragraph from './Paragraph';
+import Link from './Link';
+import Clock from './Clock';
+import Logtext from './Logtext';
+
+function App() {
+  return (
+    <div className="mx-auto p-8 mt-16 md:mt-28 w-full h-screen text-white">
+      <div>
+        <div className="text-6xl mb-3">:)</div>
+        <h1 className="mb-2 text-2xl md:text-4xl font-bold mb-4">There is no problem.</h1>
+        <Paragraph text="Hey there, I'm Mac, your everyday normal guy from Earth" />
+        <div className="mt-4 text-lg"><b>Untechnical Information:</b></div>
+        <div className="">
+        <div className=""><b>BIRTHDATE:</b> 3rd April 2008</div>
+        <div><b>FAV-SONG:</b> <Link text="night sky patrol of tomorrow" link="https://youtu.be/XogSflwXgpw"/> by orangestar</div>
+        <div><b>CONTACTS:</b> <Link text="Instagram" link="https://instagram.com/bluestar.name" />, <Link text="Discord" link="https://discord.com/users/829156179803504670" /></div>
+        <Logtext title="TEST:" content="test" />
+        <Clock />
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
+*/}
